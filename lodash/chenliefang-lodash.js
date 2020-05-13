5376d602880e10c2299fefb2d6daaa217025ae34
ChenLiefang <1350471482@qq.com>
2020-05-13 21:25:51 +0800

    -m5.13

--- lodash/chenliefang-lodash.js
+++ lodash/chenliefang-lodash.js
@@ -1,6 +1,6 @@
 var chenliefang = {
     chunk:function(array,size) {
-        let ary=[]
+        let ary = []
         for(var i = 0; i < array.length;i +=size){
             ary.push(array.slice(i,i+size))
         }
@@ -11,5 +11,47 @@ var chenliefang = {
  * @param ary {array} 需要拆分的数组
  * @param size {number} 拆分的大小
  * @return {Array[][]}
- */
+ */,
+    compact:function(array){
+        let result = []
+        for(var i = 0;i < array.length; i++){
+            if(array[i]){
+                result.push(array[i])
+            }
+        }
+        return result 
+    }
+    /**
+     * 创建一个删除了falsey值的数组 值包括false，null，0，“”，undefined， NaN
+     * @param array{array} 需要删除的数组
+     * @return 返回过滤后的数组 
+     */,
+     difference:function(array,...values){
+         let map = {};
+         let ans = [];
+         for(let i = 0; i < array.length; i ++){
+             map[array[i]] = true
+         }
+         for(let i in values){
+             for(let j in values[i]){
+                 if(map[values[i][j]]){
+                     map[values[i][j]] = false;
+                 }
+                 }
+             }
+             for(let i in array){
+                if(map[array[i]]){
+                    ans[ans.length] = array[i]
+                }
+
+         }
+         return ans 
+
+     }
+     /**
+      * 返回一个原数组中没有在其余数组中出现的元素集合
+      * @param{array} 原数组
+      * @param{...values} 其余数组
+      * @return {array[][]} 返回过滤后的数组
+      */
 }
